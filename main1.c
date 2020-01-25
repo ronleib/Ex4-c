23545512144cf2504c5c6ac38157f4423157b774
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-01-25 20:25:36 +0200

    use 1 main

--- main1.c
+++ main1.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include "frequency_lib.h"
 
-int main() {
-    node head;
-    head.count = 0;
-    for (int i=0; i < MUN_ABC; i++){
-        head.children[i] = NULL;
-    }
-    textToTree(&head);
-    for (int i = 0; i < MUN_ABC; i++) {
-        if (head.children[i] != NULL) {
-            char string[LINE] = {0};
-            printInorder(head.children[i], string, 0);
-        }
-    }
-    freeTree(&head);
-    return 0;
+int main(int argc,char *argv[]) {
+	node head;
+    	head.count = 0;
+    	for (int i=0; i < MUN_ABC; i++){
+		head.children[i] = NULL;
+	}
+	textToTree(&head);
+	if (argc == 1){
+		for (int i = 0; i < MUN_ABC; i++) {
+			if (head.children[i] != NULL) {
+				char string[LINE] = {0};
+				printInorder(head.children[i], string, 0);
+			}
+		}
+	}
+	else {
+		for (int i = MUN_ABC-1; i >= 0 ; i--) {
+			if (head.children[i] != NULL) {
+            			char string[LINE] = {0};
+            			printPreorder(head.children[i], string, 0);
+        		}
+    		}
+    	}
+    	freeTree(&head);
+    	return 0;
 }
